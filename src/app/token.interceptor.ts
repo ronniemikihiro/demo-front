32915cf0f61a302c9f81e73790ac8f5119037564
Ronnie Lopes <ronnie.lopes@esales.poa>
2020-10-21 10:43:17 -0300

    first commit

---
+++ src/app/token.interceptor.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+
+    const tokenString = localStorage.getItem('access_token');
+
+    const url = request.url;
+
+    if( tokenString && !url.endsWith('/oauth/token') ){
+      const jwt = JSON.parse(tokenString);
+      const token = jwt.access_token;
+      request = request.clone({
+        setHeaders : {
+          Authorization: 'Bearer ' + token
+        }
+      })
+    }
+
+    return next.handle(request);
+  }
+}
